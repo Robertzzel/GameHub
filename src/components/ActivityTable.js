77d64fbf03321c9be62ba7ac56068bc34d0b8b77
“Robertzzel” <robertzel_2001@yahoo.com>
2021-12-29 14:45:14 +0200

    pawns are moving

--- src/components/ActivityTable.js
+++ src/components/ActivityTable.js
@@ -4,48 +4,90 @@ import '../static/activity/css/ActivityTable.css'
 import tabla from '../static/activity/images/tabla.png'
 
 export default function ActivityTable(){
-
-    const pawnColors = ['green', 'yellow', 'red', 'blue'];
-    let nrOfPlayers = 2;
-    let pawns = [];
+    const pawnColors = ['green','blue', 'red', 'purple'];
+    let pawnOnEachPosition = Array(50).fill(0)
+    let [nrOfPlayers, setNrOfPlayers] = useState(0);
 
     let calculateCoordsFromPozition = (pozition) => {
-        if(pozition === 0) return [1,5];
-        else if(pozition === 49) return [12,5];
+        pozition = parseInt(pozition)
+        if(pozition <= 0){
+            return [1,5];
+        }
+        else if(pozition >= 49){
+            return [12,5];
+        } 
         else{
             let x = parseInt((pozition-1) / 4) + 1;
             let y = ((pozition-1) % 4) + 1;
             if(x % 2) y = 4 - y + 1;
             return [x,y];
         }
+        
+    }
+
+    let calculatePozitionFromCoords = (x,y) => {
+        x= parseInt(x)
+        y = parseInt(y)
+        if ( x === 1 && y===5 ) return 0;
+        else if( x===12 && y===5 ) return 49;
+        else{
+            let position = 4 * (x-1);
+            if (x % 2) position += 4 - (y-1);
+            else position += y;
+            return position;
+        }
+    }
+
+    let getPawnPosition = (identifier) => {
+        let pawn = document.getElementsByClassName(identifier)[0];
+        let y = pawn.style.gridColumn.split('/')[0].trim()
+        let x = pawn.style.gridRow.split('/')[0].trim()
+        return calculatePozitionFromCoords(parseInt(x),parseInt(y))
     }
 
     let setPawnPosition = (identifier,pozitie) => {
-        let pawn = document.getElementsByClassName(identifier)[0];
+        pawnOnEachPosition[getPawnPosition(identifier)]--;
+        pawnOnEachPosition[pozitie]++;
         let [x, y] = calculateCoordsFromPozition(pozitie);
-        pawn.style.gridColumn = `${x}`; 
-        pawn.style.gridRow = `${y}`;
+        let pawn = document.getElementsByClassName(identifier)[0];
+        pawn.style.gridColumn = `${y}`; 
+        pawn.style.gridRow = `${x}`;
     }
     
     useEffect(()=>{
         while(true){
-            nrOfPlayers = prompt('Numar de echipe')
-            if( ['1','2','3','4'].includes(nrOfPlayers) ){
-                nrOfPlayers = parseInt(nrOfPlayers);
+            let aux= prompt('Numar de echipe')
+            if( ['1','2','3','4'].includes(aux) ){
+                setNrOfPlayers(parseInt(aux));
                 break;
             }
         }
     },[])
 
+
     return(
-        <>
+        <div className='main-div'>
             <div className='container-div-board'>
-                <img src={tabla} className="board-image"/>
+                <img src={tabla} alt='board-img' className="board-image"/>
                 <div className='board-div'>
                    { pawnColors.slice(0,nrOfPlayers).map((color)=><Pawn key={color} identifier={color}/>) }
                 </div>
             </div>
-            <button onClick={()=>setPawnPosition('yellow',5)}>AS</button>
-        </>
+            <div className='d-flex flex-column' style={{margin: '15px'}}>
+                { pawnColors.slice(0,nrOfPlayers).map((color)=>
+                    <div key={`${color}Form`} className='d-flex flex-row' style={{marginTop: '10px'}}>
+                        <h2>{color}</h2>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)+1))} className='btn btn-lg btn-success' style={{marginRight: '10px'}}>+1</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)+3))} className='btn btn-lg btn-success' style={{marginRight: '10px'}}>+3</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)+4))} className='btn btn-lg btn-success' style={{marginRight: '10px'}}>+4</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)+5))} className='btn btn-lg btn-success' style={{marginRight: '10px'}}>+5</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)-1))} className='btn btn-lg btn-danger' style={{marginRight: '10px'}}>-1</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)-3))} className='btn btn-lg btn-danger' style={{marginRight: '10px'}}>-3</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)-4))} className='btn btn-lg btn-danger' style={{marginRight: '10px'}}>-4</button>
+                        <button onClick={()=>setPawnPosition(color,(getPawnPosition(color)-5))} className='btn btn-lg btn-danger' style={{marginRight: '10px'}}>-5</button>
+                    </div>
+                ) }
+            </div>
+        </div>
     )
 }
--- src/components/Pawn.js
+++ src/components/Pawn.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import '../static/activity/css/Pawn.css'
+import greenPawnImage from '../static/activity/images/pioni/greenPawn.png'
+import bluePawnImage from '../static/activity/images/pioni/bluePawn.png'
+import purplePawnImage from '../static/activity/images/pioni/purplePawn.png'
+import redPawnImage from '../static/activity/images/pioni/redPawn.png'
 
 export default function ActivityTable(props){
-    const style={
+    let divStyle={
         gridRow: `1`,
-        gridColumn: `5`
+        gridColumn: `5`,
+        maxWidth: '30%',
+        maxHeight: '80%',
+        position: 'relative',
+        left: '10%'
     }
+    const imageStyle={
+        width: '100%',
+        height:'100%',
+    }
+
+    let correctPawnImage;
+    switch(props.identifier){
+        case 'green':
+            correctPawnImage = greenPawnImage;
+            divStyle.left = '15%';
+            break;
+        case 'blue':
+            correctPawnImage = bluePawnImage;
+            divStyle.left = '30%';
+            break;
+        case 'purple':
+            correctPawnImage = purplePawnImage;
+            divStyle.left = '45%';
+            break;
+        case 'red':
+            correctPawnImage = redPawnImage;
+            divStyle.left = '60%';
+            break;
+        case 'yellow':
+            correctPawnImage = purplePawnImage;
+            divStyle.left = '75%';
+            break;
+        default:
+            correctPawnImage = purplePawnImage;
+            divStyle.left = '75%';
+    }
+
     return(
-        <div className={'pawn ' + props.identifier} style={style}>
-            <p>{props.pozitie}</p>
+        <div className={'pawn ' + props.identifier} style={divStyle}>
+            <img src={correctPawnImage} alt='pawn-img' style={imageStyle}></img>
         </div>
     )
 }
